69f451c127a27a2b88b5020ee5450f6a510edde5
Ggowda2004 <kmgangadhargowda2004@gmail.com>
2026-02-18 23:47:59 +0530

    added hash and verify password, generate and verify jwt tokens

---
+++ backend/app/core/exception.py
@@ -0,0 +1,3 @@
+def AuthError(Exception):
+    #Raised and used when there is an authentication error. this is used because core, cannot be dependent on other files hence must not imprort from other files to handel this exception
+    pass
